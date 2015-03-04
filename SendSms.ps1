85aff2cd0da268c372328e7b469b258c162a5598
Quynh Nguyen <qn@rosslynanalytics.com>
2015-03-04 15:57:23 +0000

    First check in

---
+++ SendSms.ps1
@@ -0,0 +1,3 @@
+﻿Add-Type -path "Twilio.Api.dll"
+$twilio = new-object Twilio.TwilioRestClient("#TwilioAccountId", "#AuthenticationToken")
+$msg = $twilio.SendSmsMessage("#FromNumber", "#ToNumber", "London Bridge is falling down my fair lady!")
